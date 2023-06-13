f0bb119991aab76127839f40e010895525d8f7d8
Onur Temiz <onurmain@gmail.com>
2023-06-13 13:11:38 +0300

    create token and user middleware for menu authorization

--- controllers/menuPage.js
+++ controllers/menuPage.js
@@ -2,22 +2,11 @@ const menuRouter = require('express').Router();
 const Menu = require('../models/menu');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const { tokenExtractor, userExtractor } = require('../utils/middleware');
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('bearer ')) {
-    return authorization.replace('bearer ', '')
-  }
-  return null
-}
-
-menuRouter.post('/', async (request, response) => {
+menuRouter.post('/', tokenExtractor, userExtractor, async (request, response) => {
   const body = request.body;
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'invalid token' })
-  }
-  const user = await User.findById(decodedToken.id);
+  const user = request.user;
 
   const menu = new Menu({
     user: user.id,
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
 const requestLogger = (request, response, next) => {
     console.log('Method: ', request.method);
     console.log('Path: ', request.path);
@@ -18,7 +21,30 @@ const errorHandler = (error, request, response, next) => {
     next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+    if (!authorization) {
+        return response.status(401).json({ error: 'Unauthorized' })
+    } else if (authorization && authorization.startsWith('bearer ')) {
+        const token = authorization.replace('bearer ', '')
+        request.token = token
+    }
+    next()
+}
+
+const userExtractor = async (request, response, next) => {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'invalid token' })
+    }
+    const user = await User.findById(decodedToken.id)
+    request.user = user
+    next()
+}
+
 module.exports = {
     requestLogger,
     errorHandler,
+    tokenExtractor,
+    userExtractor,
 }
